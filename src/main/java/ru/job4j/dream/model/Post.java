b856f443c0ee9b4132d7f9efb6600afa0fedb75e
RaduKostashchuk <kostasc@mail.ru>
2021-12-12 16:55:37 +0700

    1. Servlet. Web.xml [#6866]

--- src/main/java/ru/job4j/dream/model/Post.java
+++ src/main/java/ru/job4j/dream/model/Post.java
@@ -9,6 +9,11 @@ public class Post {
     private String description;
     private LocalDateTime created;
 
+    public Post(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public Post(int id, String name, String description, LocalDateTime created) {
         this.id = id;
         this.name = name;
